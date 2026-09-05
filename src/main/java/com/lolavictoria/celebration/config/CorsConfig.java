63276b3a6600f0c459d12e7dc6a5abdc0673ec87
LolaVictoria <damilolaoniyide11@gmail.com>
2026-09-05 23:32:26 +0100

    udated cors with deployed frontend link

--- src/main/java/com/lolavictoria/celebration/config/CorsConfig.java
+++ src/main/java/com/lolavictoria/celebration/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173",
+                                "https://memoria-frontend-v1.vercel.app/"
                         )
                         .allowedMethods(
                                 "GET",
